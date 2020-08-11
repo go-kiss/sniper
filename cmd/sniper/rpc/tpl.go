c4087c64b73d7bd3b6654ebc60e1b87d735b87d2
吕海涛 <ihaitao@qq.com>
2020-08-11 18:55:29 +0800

    漏提交了一个文件
    sniper 目前录实 git 忽略了……

---
+++ cmd/sniper/rpc/tpl.go
@@ -0,0 +1,88 @@
+package rpc
+
+var serverTpl = `
+package {{.ServerPkg}}
+
+import (
+	"context"
+
+	pb "{{.RPCPkg}}"
+)
+
+type {{.Service}}Server struct{}
+`
+
+var funcTpl = `
+func (s *{{.Service}}Server) {{.Name}}(ctx context.Context, req *pb.{{.ReqType}}) (resp *pb.{{.RespType}}, err error) {
+	// FIXME 请开始你的表演
+	return
+}
+`
+
+var echoFuncTpl = `
+func (s *{{.Service}}Server) Echo(ctx context.Context, req *pb.{{.Service}}EchoReq) (resp *pb.{{.Service}}EchoResp, err error) {
+	return &pb.{{.Service}}EchoResp{Msg: req.Msg}, nil
+}
+`
+
+var regServerTpl = `
+package main
+func main() {
+	{
+		server := &{{.Server}}server{{.Version}}.{{.Service}}Server{}
+		handler := {{.Server}}_v{{.Version}}.New{{.Service}}Server(server, {{.Hooks}})
+		mux.Handle({{.Server}}_v{{.Version}}.{{.Service}}PathPrefix, handler)
+	}
+}
+`
+
+var importTpl = `
+package main
+import(
+	{{.PKGName}} {{.RPCPath}}
+	{{.ServerPath}}
+)
+`
+
+func initNewTpl() {
+	serverTpl = `
+package {{.Server}}_v{{.Version}}
+
+import (
+	"context"
+)
+
+type {{.Service}}Server struct{}
+`
+
+	funcTpl = `
+func (s *{{.Service}}Server) {{.Name}}(ctx context.Context, req *{{.ReqType}}) (resp *{{.RespType}}, err error) {
+	// FIXME 请开始你的表演
+	return
+}
+`
+
+	echoFuncTpl = `
+func (s *{{.Service}}Server) Echo(ctx context.Context, req *{{.Service}}EchoReq) (resp *{{.Service}}EchoResp, err error) {
+	return &{{.Service}}EchoResp{Msg: req.Msg}, nil
+}
+`
+
+	regServerTpl = `
+package main
+func main() {
+	{
+		server := &{{.Server}}_v{{.Version}}.{{.Service}}Server{}
+		handler := {{.Server}}_v{{.Version}}.New{{.Service}}Server(server, {{.Hooks}})
+		mux.Handle({{.Server}}_v{{.Version}}.{{.Service}}PathPrefix, handler)
+	}
+}
+`
+
+	importTpl = `
+package main
+import(
+	{{.PKGName}} {{.RPCPath}}
+)
+`
+}
