2ba5e5a5517a81abfd1e686241ff01006d252fcf
王真 <1046060018@qq.com>
2020-11-18 14:36:57 +0800

    adjust 1e6 float to 1000000 int

--- util/twirp/hooks.go
+++ util/twirp/hooks.go
@@ -142,9 +142,8 @@ func (h *ServerHooks) WriteError(ctx context.Context, resp http.ResponseWriter, 
 func marshalErrorToJSON(twerr Error) []byte {
 	// make sure that msg is not too large
 	msg := twerr.Msg()
-	maxLen := int(1e6)
-	if len(msg) > maxLen {
-		msg = msg[:maxLen]
+	if len(msg) > 1000000 {
+		msg = msg[:1000000]
 	}
 
 	type twerrJSON struct {
