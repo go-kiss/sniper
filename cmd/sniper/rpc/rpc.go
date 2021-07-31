2efeb4f010e9c8a4b657f606ac22b9adcdc6623d
吕海涛 <ihaitao@qq.com>
2021-07-31 19:54:11 +0800

    移除默认的 proto 行尾注释
    现在支持针对特定 server 和 method 设置 hooks，之前的 MethodOption
    方式不再推荐使用。

--- cmd/sniper/rpc/rpc.go
+++ cmd/sniper/rpc/rpc.go
@@ -14,12 +14,7 @@ package {{.Server}}.v{{.Version}};
 // FIXME 服务必须写注释
 service {{.Service}} {
     // FIXME 接口必须写注释
-    //
-    // 这里的行尾注释 sniper:foo 有特殊含义，是可选的
-    // 框架会将此处冒号后面的值(foo)注入到 ctx 中，
-    // 用户可以使用 twirp.MethodOption(ctx) 查询，并执行不同的逻辑
-    // 这个 sniper 前缀可以通过 --twirp_out=option_prefix=sniper:. 自定义
-    rpc Echo({{.Service}}EchoReq) returns ({{.Service}}EchoResp); // sniper:foo
+    rpc Echo({{.Service}}EchoReq) returns ({{.Service}}EchoResp);
 }
 
 message {{.Service}}EchoReq {
